3998f516e49a46d5f9b4b216cb7d4d4b27cbc34c
deevee <github@vansteenwegen.org>
2021-04-13 12:09:13 +0200

    Added line endings after each line

--- tiddly_consumables.py
+++ tiddly_consumables.py
@@ -7,10 +7,10 @@ Tiddlywiki: https://tiddlywiki.com/
 import sys  # to check python version
 # for easy access to clipboard use tkinter, but package name differs between python v2 and v3. First check.
 if sys.version_info.major == 3:
-    from tkinter import Tk  
+    from tkinter import Tk
 else:
     from Tkinter import Tk
-    
+
 
 def get_line():
     what = input("What:").strip()
@@ -20,15 +20,18 @@ def get_line():
     url = input('URL (enter for none): ').strip() or None
     price = input('Price: ').strip() or ''
     currency = input('Currency (enter for Euro): ') or None
-    quantity = input('Quantity (enter for single): ' ).strip() or 1
+    quantity = input('Quantity (enter for single): ').strip() or 1
     if url:
         part_no = '[[{}|{}]]'.format(part_no, url)
-    price =  '{} {}'.format(price, currency) if currency else '€{}'.format(price)
+    price = '{} {}'.format(price,
+                           currency) if currency else '€{}'.format(price)
     if not quantity == 1:
         price = '{}/{}'.format(price, quantity)
-    rtn = '| {} |{} | {} | {} | {} |'.format(what, product, supplier, part_no, price)
+    rtn = '| {} |{} | {} | {} | {} |\n\r'.format(what, product, supplier, part_no,
+                                             price)
     another = input('Another product? (y for another, enter to get result)')
     return rtn, another
+
 
 def print_and_clipboard(result):
     r = Tk()
@@ -39,17 +42,19 @@ def print_and_clipboard(result):
     r.destroy()
     print(result)
 
+
 def main():
     result = ''
     another = True
 
     while another:
         line, another = get_line()
-        result = result + line
+        result += line
 
     print_and_clipboard(result)
     # exit
     input('Data copied to clipboard, press enter to exit...')
+
 
 if __name__ == '__main__':
     main()
